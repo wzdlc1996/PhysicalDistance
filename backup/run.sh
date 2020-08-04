cf36f87aa4c75ce64fa8f09ce2321b6ed5149ca6
wzdlc1996 <wzdlc1996@gmail.com>
2020-08-04 23:24:15 +0800

    update

--- backup/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-~/anaconda3/bin/python kickedRotor.py 5.0 10
-~/anaconda3/bin/python kickedRotor.py 5.0 14
-~/anaconda3/bin/python kickedRotor.py 5.0 18
-~/anaconda3/bin/python kickedRotor.py 5.0 20
-~/anaconda3/bin/python kickedRotor.py 5.0 24
-~/anaconda3/bin/python kickedRotor.py 5.0 28
